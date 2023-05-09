73580bf7e78c4c2ac258e65aaf853ca15426a5e9
고동환\eurdr <eurdream98@naver.com>
2023-05-09 15:34:35 +0900

    중간고사 과제 완료

--- src/main/java/kr/ac/sahmyook/home/func/kr/ac/sahmyook/home/func/DowhileSample.java
+++ src/main/java/kr/ac/sahmyook/home/func/kr/ac/sahmyook/home/func/DowhileSample.java
@@ -3,10 +3,20 @@ package kr.ac.sahmyook.home.func.kr.ac.sahmyook.home.func;
 import java.util.Scanner;
 
 public class DowhileSample {
+    Scanner sc = new Scanner(System.in);
+
     public void addDashToken() {
+        System.out.print("문자열을 입력하세요: ");
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i));
+            if (i < str.length() - 1) {
+                System.out.print("-");
+            }
+        }
     }
 
-    ;
 
     public void burgerKingMenu() {
         Scanner sc = new Scanner(System.in);
@@ -23,6 +33,22 @@ public class DowhileSample {
     ;
 
     public void isStringAlphabet() {
+        System.out.print("문자열을 입력하세요: ");
+        String str = sc.nextLine();
+
+        boolean isAllAlphabet = true;
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isAlphabetic(str.charAt(i))) {
+                isAllAlphabet = false;
+                break;
+            }
+        }
+
+        if (isAllAlphabet) {
+            System.out.println("모든 글자가 영문자입니다.");
+        } else {
+            System.out.println("영문자가 아닌 글자가 포함되어 있습니다.");
+        }
     }
 
     ;
